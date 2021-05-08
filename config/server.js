f8fa961bc7b3a68c4cfad284c376f412e09a1ce9
Eder R. Fleming <ederfleming@gmail.com>
2021-05-08 18:45:42 -0300

    Initial Commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '3427f3700c3a12d8beec06063da11c35'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '5c3357b1-74c1-4ba3-9bca-54ac1f75e8bb'
+};
